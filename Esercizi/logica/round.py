a34f1c921efd428910d376c57d52095d3746a8e8
Daniele Cerrina <daniecerri@gmail.com>
2025-10-30 20:16:46 +0100

    Lezione 30/10/2025

---
+++ Esercizi/logica/round.py
@@ -0,0 +1,11 @@
+"""
+Implementare manualmente la funzione round:
+Dato un numero e il numero di cifre, fare l'arrotondamento opportuno
+"""
+import random
+numero = random.uniform(1e-5, 1e2)
+cifre = random.randint(-5, 5)
+
+print(f"Numero: {numero}, cifre: {cifre}")
+
+arrondamento = ...
--- Soluzioni/Esercizio_dizionari_e_set.py
+++ Soluzioni/Esercizio_dizionari_e_set.py
@@ -70,3 +70,61 @@ for chiave_prima, elemento_primo in ricette.items():
     for chiave_seconda, elemento_secondo in ricette.items():
         if len(elemento_primo & elemento_secondo) == 0:
             print(f"'{chiave_prima}' non ha nessun ingrediente in comune con '{chiave_seconda}'")
+print()
+
+## Esercizio 8
+# Crea un dizionario `prezzi` con il costo di ogni ingrediente e
+# calcola quanto costa preparare ogni ricetta.
+# Stampa le ricette ordinate dal costo più basso al più alto.
+print(f"{sep} Esercizio 8 {sep}")
+
+# Per l'esercizio faremo una generazione casuale dei prezzi per ogni ingrediente
+
+### 1. Bisogna capire quali sono tutti gli ingredienti UNICI necessari
+# Per fare questo, facciamo l'unione di tutti i set con gli ingredienti delle ricette
+ingredienti_totali = set()  # Lo inzializziamo a set vuoto
+for ingredienti in ricette.values():  # Per ogni set di ingredienti
+    # Calcoliamo l'unione con gli ingredienti già trovati
+    ingredienti_totali = ingredienti_totali | ingredienti
+
+print(f"Ingredienti unici: {ingredienti_totali}")
+
+### 2. Facciamo il dizionario con i prezzi casuali
+diz_prezzi_ingredienti = {}  # Creiamo il dizionario vuoto
+
+# Per ogni ingrediente unico
+for ingrediente in ingredienti_totali:
+    # Estraiamo il prezzo
+    prezzo = random.uniform(1, 10)
+    # Associamo all'ingrediente il suo prezzo NEL DIZIONARIO
+    diz_prezzi_ingredienti[ingrediente] = round(prezzo, 2)
+
+print(f"Prezzi degli ingredienti: {diz_prezzi_ingredienti}")
+
+# Calcoliamo il costo di ogni ricetta
+for ricetta, ingredienti in ricette.items():  # Per ogni ricetta
+    # 1. Capiamo quali sono gli ingredienti di cui dobbiamo andare a guardare il prezzo
+    tot = 0
+    prezzi = {}
+    # Gli ingredienti di cui dobbiamo guardare il prezzo sono TUTTI quelli della ricetta
+    # NON facciamo un for su tutti gli ingredienti di cui abbiamo un prezzo
+    # MA solamente su quelli di cui siamo sicuri ci serva saperlo
+    for ingrediente in ingredienti:  # Per ogni ingrediente della ricetta
+        costo = diz_prezzi_ingredienti[ingrediente]  # Otteniamo il suo prezzo
+        tot += costo  # Lo aggiungiamo al totale
+        prezzi[ingrediente] = costo  # Per DEBUG teniamo traccia di tutti i prezzi ottenuti
+
+    # Stampiamo il totale della ricetta
+    print(f"Costo per {ricetta}: {tot:.2f}")
+    print(f"  - Dettaglio: {prezzi}")
+
+print()
+print(f"{sep} Esercizio BONUS {sep}")
+
+print(ingredienti_totali - dispensa)
+
+
+
+
+
+
